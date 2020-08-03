43c603fd66dac875475301766eeb9fe54e7188ac
Golam Mostofa <golammostofa31188@gmail.com>
2020-08-04 01:24:45 +0600

    SearchController fixed for category search page, and theme color changes to pink also changes width of search widget

--- override/controllers/front/listing/SearchController.php
+++ override/controllers/front/listing/SearchController.php
@@ -1,28 +1,5 @@
 <?php
-/**
- * 2007-2019 PrestaShop and Contributors
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * https://opensource.org/licenses/OSL-3.0
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@prestashop.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to https://www.prestashop.com for more information.
- *
- * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2019 PrestaShop SA and Contributors
- * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
- * International Registered Trademark & Property of PrestaShop SA
- */
+
 use PrestaShop\PrestaShop\Adapter\Search\SearchProductSearchProvider;
 use PrestaShop\PrestaShop\Core\Product\Search\ProductSearchQuery;
 use PrestaShop\PrestaShop\Core\Product\Search\SortOrder;
@@ -55,16 +32,16 @@ class SearchController extends SearchControllerCore
             ));
             $this->setTemplate($template, $params, $locale);
         }
-    }*/
+    }
 
     public function getAjaxProductSearchVariables()
     {
         $search = $this->getProductSearchVariables();
-        /*
+        
         $rendered_products_top = $this->render('catalog/_partials/products-top', array('listing' => $search));
         $rendered_products = $this->render('catalog/_partials/products', array('listing' => $search));
         $rendered_products_bottom = $this->render('catalog/_partials/products-bottom', array('listing' => $search));
-        */
+        
         $data = array_merge(
             array(
                 'rendered_products_top' => null,
@@ -81,7 +58,7 @@ class SearchController extends SearchControllerCore
         }
 
         return $data;
-    }
+    }*/
 
    
 }
